d6f67ee8ac01c68c80e19b70a9b3cb546d2d830b
Mark Steffensen <mark.steffensen@microfocus.com>
2022-04-01 11:31:42 -0600

    Update InstallDockerOnNimbusClient.ps1

--- InstallDockerOnNimbusClient.ps1
+++ InstallDockerOnNimbusClient.ps1
@@ -54,13 +54,14 @@ if (Should-Run-Step "B")
 	
 	Write-Host "----------"	
 	Write-Host "Installing nimbusapp ..."
-    curl.exe -L https://raw.githubusercontent.com/msteffensen19/InstallNimbusappEverywhere/main/nimbusapp_1.6.0_ms > "..\nimbusapp_1.6.0_ms.pl"
-    curl.exe -L https://raw.githubusercontent.com/msteffensen19/InstallNimbusappEverywhere/main/Windows/InstallNimbusapp.bat > ".\InstallNimbusapp.bat"
-    start-process .\InstallNimbusapp.bat
-    del ..\nimbusapp_1.6.0_ms.pl .\InstallNimbusapp.bat
+	cd C:\
+    git clone https://github.com/msteffensen19/InstallNimbusappEverywhere.git
+    cd C:\InstallNimbusappEverywhere\Windows
+    .\InstallNimbusapp.bat
 	
 	Write-Host "----------"	
 	Write-Host "Installing NimbusAliasesEverywhere ..."
+	cd C:\
     git clone https://github.com/msteffensen19/InstallNimbusAliasesEverywhere.git
     cd C:\InstallNimbusAliasesEverywhere\Windows
     .\InstallNimbusAliases.bat 
