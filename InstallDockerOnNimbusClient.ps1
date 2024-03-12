bde2ae5dc6e9ba197691b3ea100213f93d140287
Mark Steffensen <msteffensen19@gmail.com>
2024-03-12 16:08:26 -0600

    Update InstallDockerOnNimbusClient.ps1

--- InstallDockerOnNimbusClient.ps1
+++ InstallDockerOnNimbusClient.ps1
@@ -39,7 +39,7 @@ if (Should-Run-Step "B")
     # Start-Service Docker
     # Enable-WindowsOptionalFeature -Online -FeatureName Containers
     Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1" -o install-docker-ce.ps1
-    .\install-docker-ce.ps1
+    .\install-docker-ce.ps1 -DockerVersion 24.0.7
     
         Write-Host "----------"
 	Write-Host "Updating LRE Chrome bookmarks ..."
